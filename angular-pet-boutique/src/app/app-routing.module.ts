09a8a28c4235a1aa4f538a0201dd881c22f58ec4
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-11-21 15:17:22 -0300

    Carousel Import OK

--- angular-pet-boutique/src/app/app-routing.module.ts
+++ angular-pet-boutique/src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './view/home/home.component';
 
 const routes: Routes = [
   {
-    path: "header",
-    component: HeaderComponent
-  },
-  {
-    path: "header",
-    component: FooterComponent
+    path: '',
+    component: HomeComponent
   }
 ];
 
--- angular-pet-boutique/src/app/app.module.ts
+++ angular-pet-boutique/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CarouselModule } from '@coreui/angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HomeComponent } from './view/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
