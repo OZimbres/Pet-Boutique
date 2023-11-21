24aff552d756e951a4cea27c01ec6e214d636d24
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-11-21 14:27:35 -0300

    Rolving Router Problem

--- angular-pet-boutique/src/app/app-routing.module.ts
+++ angular-pet-boutique/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "header",
+    component: HeaderComponent
+  },
+  {
+    path: "header",
+    component: FooterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
