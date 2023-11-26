b65e9d05e401ec93ed2dabfb791a471c2c787108
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-11-25 21:58:30 -0300

    Produto's page structure done

--- angular-pet-boutique/src/app/shared/produtos-filtros/produtos-filtros.component.ts
+++ angular-pet-boutique/src/app/shared/produtos-filtros/produtos-filtros.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-produtos-filtros',
   templateUrl: './produtos-filtros.component.html',
-  styleUrls: ['./produtos-filtros.component.css']
+  styleUrls: ['./produtos-filtros.component.scss']
 })
 export class ProdutosFiltrosComponent {
 
--- angular-pet-boutique/src/app/shared/produtos-resultados/produtos-resultados.component.ts
+++ angular-pet-boutique/src/app/shared/produtos-resultados/produtos-resultados.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-produtos-resultados',
   templateUrl: './produtos-resultados.component.html',
-  styleUrls: ['./produtos-resultados.component.css']
+  styleUrls: ['./produtos-resultados.component.scss']
 })
 export class ProdutosResultadosComponent {
 
