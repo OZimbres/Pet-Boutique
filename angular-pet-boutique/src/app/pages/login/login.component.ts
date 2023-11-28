076c92328fee9e8d48ed515ae64d3efb213292b6
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-11-28 14:22:38 -0300

    Chancging login Typescript

--- angular-pet-boutique/src/app/pages/login/login.component.ts
+++ angular-pet-boutique/src/app/pages/login/login.component.ts
@@ -1,28 +1,9 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements AfterViewInit {
-  @ViewChild('signin') btnSignin!: ElementRef<HTMLButtonElement>;
-  @ViewChild('signup') btnSignup!: ElementRef<HTMLButtonElement>;
-  @ViewChild('body') body!: ElementRef<HTMLDivElement>;
-
-  ngAfterViewInit(): void {
-    this.setupEventListeners();
-  }
-
-  private setupEventListeners(): void {
-    if (this.btnSignin && this.btnSignup && this.body) {
-      this.btnSignin.nativeElement.addEventListener('click', () => {
-        this.body.nativeElement.className = 'sign-in-js';
-      });
-
-      this.btnSignup.nativeElement.addEventListener('click', () => {
-        this.body.nativeElement.className = 'sign-up-js';
-      });
-    }
-  }
+export class LoginComponent {
 }
